513c2bf92445c79ae4f1cd4eaba6caaa6588a596
Brittaktb <brittaktb@web.de>
2025-09-08 16:30:00 +0200

    correctd staticfiles path in settings

--- config/settings.py
+++ config/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,8 +26,9 @@ SECRET_KEY = "django-insecure-qrgmty(crn&qb7np#fxmt(t*+n51g1bdbl$oj)#m@n+a^@0!zl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*",
-    "brittaktb.pythonanywhere.com"
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "brittaktb.pythonanywhere.com",    
 ]
 
 
@@ -119,9 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/" 
+STATICFILES_DIRS = []
 
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
